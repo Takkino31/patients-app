34c8fd6b9bbbe5cc48a0f916f81ced31f6174102
Takkino31 <takkinoyaya@gmail.com>
2025-01-26 12:16:33 +0000

    Delete Medecin DONE

--- src/main/java/takkino/java/patientsapp/web/MedecinController.java
+++ src/main/java/takkino/java/patientsapp/web/MedecinController.java
@@ -3,6 +3,7 @@ package takkino.java.patientsapp.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import takkino.java.patientsapp.entities.Medecin;
 import takkino.java.patientsapp.repositories.MedecinRepository;
 
@@ -20,5 +21,11 @@ public class MedecinController {
         Collection <Medecin> medecins =  medecinRepository.findAll();
         model.addAttribute("medecins", medecins);
         return "medecins";
+    }
+
+    @GetMapping("/deleteMedecin")
+    public String deleteMedecin(@RequestParam (name="id")Long id) {
+        medecinRepository.deleteById(id);
+        return "redirect:/medecins";
     }
 }
