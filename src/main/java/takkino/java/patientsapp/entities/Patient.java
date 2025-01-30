376504770806822c72d1926adec737bf7dc63823
Takkino31 <takkinoyaya@gmail.com>
2025-01-30 19:50:05 +0000

    Validation Patient DONE

--- src/main/java/takkino/java/patientsapp/entities/Patient.java
+++ src/main/java/takkino/java/patientsapp/entities/Patient.java
@@ -1,6 +1,9 @@
 package takkino.java.patientsapp.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -15,12 +18,20 @@ import java.util.Date;
 public class Patient {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty @Size(min = 3, max = 50)
     private String firstName;
+
+    @NotEmpty @Size(min = 3, max = 50)
     private String lastName;
+
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateOfBirth;
+
     private boolean sick;
+
+    @DecimalMin("100")
     private int score;
 
 }
--- src/main/java/takkino/java/patientsapp/web/PatientController.java
+++ src/main/java/takkino/java/patientsapp/web/PatientController.java
@@ -1,9 +1,11 @@
 package takkino.java.patientsapp.web;
 
+import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,7 +56,8 @@ public class PatientController {
     }
 
     @PostMapping("/save")
-    public String savePatient(@ModelAttribute("patient") Patient patient){
+    public String savePatient(@Valid @ModelAttribute("patient") Patient patient, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) return "formPatients";
         patientRepository.save(patient);
         return "redirect:/formPatients";
     }
